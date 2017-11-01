79d4f9c92185bbc61f36417e42e447e97ad709d4
Karl Xu <karl.yogi.xu@gmail.com>
2017-11-02 01:53:29 +0800

    add script  && update image

---
+++ scripts/install-flannel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.0/Documentation/kube-flannel.yml
--- terraform/alicloud.tf
+++ terraform/alicloud.tf
@@ -11,7 +11,7 @@ resource "alicloud_key_pair" "key_pair" {
 
 resource "alicloud_instance" "web" {
     availability_zone          = "${var.availability_zone}"
-    image_id                   = "m-t4nh8g1nh0vbqr2reuvl"
+    image_id                   = "m-t4n2pbmzw5625m9ggvxv"
     
     instance_type              = "ecs.n1.medium"
     is_outdated                = true
