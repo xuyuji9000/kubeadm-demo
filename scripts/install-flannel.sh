12bee7b8cdbffb788eb15c097ed7d86bc58b0ef9
Karl Xu <karl.yogi.xu@gmail.com>
2017-11-02 13:54:37 +0800

    alicloud flannel script

--- scripts/install-flannel.sh
+++ scripts/install-flannel.sh
@@ -2,9 +2,7 @@
 
 echo "127.0.0.1 $(hostname)" >> /etc/hosts
 
-# kubeadm init --pod-network-cidr=10.244.0.0/16
-
-kubeadm init --pod-network-cidr=10.24.0.0/16
+kubeadm init --pod-network-cidr=10.244.0.0/16
 
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
@@ -13,4 +11,3 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.0/Documentation/kube-flannel.yml
-#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel-aliyun.yml
