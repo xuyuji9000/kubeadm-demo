15a728d885d6678857744e326abcfed40b121235
Karl Xu <karl.yogi.xu@gmail.com>
2017-11-02 00:51:41 -0500

    Update install-flannel.sh

--- scripts/install-flannel.sh
+++ scripts/install-flannel.sh
@@ -2,7 +2,9 @@
 
 echo "127.0.0.1 $(hostname)" >> /etc/hosts
 
-kubeadm init --pod-network-cidr=10.244.0.0/16
+# kubeadm init --pod-network-cidr=10.244.0.0/16
+
+kubeadm init --pod-network-cidr=10.24.0.0/16
 
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
